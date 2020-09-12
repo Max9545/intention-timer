43611d34c2da4258411314a598638ab0e2999a37
Jordon Smith <jordon00smith@gmail.com>
2020-09-11 20:50:19 -0600

    Change Current Activity panel styling

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ var meditateSelectedImage = document.querySelector('.meditate-selected-image');
 var exercisePreselectImage = document.querySelector('.exercise-preselect-image');
 var exerciseSelectedImage = document.querySelector('.exercise-selected-image');
 var studyColors = document.querySelector('.study-colors');
+var goalBox = document.querySelector('.input-box1');
 var minBox = document.querySelector('.input-box2');
 var secBox = document.querySelector('.input-box3');
 var startButton = document.querySelector('.start-btn');
 var currentActivity = document.querySelector('.current-activity');
 var newActivityBox = document.querySelector('.new-activity-box');
+var goal = document.querySelector('.place-holder');
 var startTimerButton = document.querySelector('.start-button');
 
 var activity;
@@ -26,9 +28,9 @@ startButton.addEventListener('click',startActivity);
 startTimerButton.addEventListener('click', countdown);
 
 function startActivity() {
-  formHider();
-  console.log('hi');
+ formHider();
  activity = new Activity ({});
+ displayGoal();
 }
 
 function formHider() {
@@ -89,8 +91,12 @@ function addExerciseColor(elements) {
   }
 }
 
+function displayGoal() {
+    goal.innerText = goalBox.value;
+}
+
 function countdown(minutes, seconds) {
-    var seconds = 60;
+    var seconds = seconds;
     var mins = minutes;
     function tick() {
         //This script expects an element with an ID = "counter". You can change that to what ever you want. 
@@ -102,7 +108,7 @@ function countdown(minutes, seconds) {
             setTimeout(tick, 1000);
         } else {
             if(mins > 1){
-                countdown(mins-1);           
+                countdown(mins-1, seconds-1);
             }
         }
     }
